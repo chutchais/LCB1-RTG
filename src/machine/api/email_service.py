2a919546048bd91967de8731a07dc94686da0969
chutchais <chutchai.s@gmail.com>
2026-05-13 13:56:10 +0700

    Set productivity report if run on 8am it will generate data of yesterday

--- src/machine/api/email_service.py
+++ src/machine/api/email_service.py
@@ -33,14 +33,14 @@ def send_rtg_productivity_report(
     current_hour = now_tz.hour
     
     # ✅ Smart date logic
-    if 9 <= current_hour < 20:
+    mail_topic_shift = f'created on {report_date.strftime("%d-%b-%Y %H:%M")}'
+    if current_hour== 8 :# if run at 08:00 to 08:59, send yesterday's report
         report_date = (now_tz - timedelta(days=1)).date()
         logger.info(f"📅 Morning run: Sending yesterday's report ({report_date})")
-        mail_topic_shift = 'Morning and Night shifts' 
     else:
         report_date = now_tz.date()
         logger.info(f"📅 Evening run: Sending today's report ({report_date})")
-        mail_topic_shift = 'Morning' 
+    
     try:
         # ✅ Get productivity report (SAME as views.productivity_report_daily_html)
         logger.info(f"📊 Fetching report for {report_date}")
